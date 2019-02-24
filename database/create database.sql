f6858f96d3c700e5d43a0b75cb2241343b99f0cc
Paxon96 <bwloczyk@gmail.com>
2019-02-24 17:41:00 +0100

    Change database structure

--- database/create database.sql
+++ database/create database.sql
@@ -5,7 +5,18 @@ CREATE TABLE `words` (
 	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`pl_word` TEXT NOT NULL,
 	`en_word` TEXT NOT NULL,
+    `add_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;
+
+CREATE TABLE `users` (
+	`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`login` TINYTEXT NOT NULL,
+	`pass` TINYTEXT NOT NULL,
+    `permission` TINYTEXT NOT NULL,
+    primary key (`id`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
--- database/insert.sql
+++ database/insert.sql
@@ -1,3 +1,7 @@
 insert into words (pl_word, en_word) values
 ('kot','cat'),
 ('pies','dog')
+
+insert into users (login,pass,permission) values
+('admin','$2a$10$w71.U2ZkHevQNl5qTaI/O.y/Sqj47386tNlSiqfpxAzxP5KVVKnZG', 'ADMIN'),
+('marlena','$2a$10$zpKLcK38WfHHdZldLfp2cuRto5eNbJpSPqpbI0rvTQVmX7UpsPKmK', 'USER')
